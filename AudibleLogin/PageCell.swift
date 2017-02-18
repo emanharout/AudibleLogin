de9be69b01ede377552e04de44e218646c187a62
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-02-18 09:56:41 -0800

    Create PageCell

--- AudibleLogin/PageCell.swift
+++ AudibleLogin/PageCell.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2017 Emmanuoel Haroutunian. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PageCell: UICollectionViewCell {
+  
+  let imageView: UIImageView = {
+    let iv = UIImageView()
+    iv.image = UIImage(named: "page1")
+     
+    iv.contentMode = .scaleAspectFill
+    iv.clipsToBounds = true
+    return iv
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    setupViews()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setupViews() {
+    addSubview(imageView)
+    
+    imageView.anchorToTop(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor)
+  }
+  
+}
