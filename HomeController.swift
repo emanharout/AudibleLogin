d086ffb093375a6e4cf1a94813f68994fa14c182
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-02-23 08:11:34 -0800

    Implement sign out functionality

---
+++ HomeController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeController.swift
+//  AudibleLogin
+//
+//  Created by Emmanuoel Haroutunian on 2/22/17.
+//  Copyright © 2017 Emmanuoel Haroutunian. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    navigationItem.title = "We're logged in"
+    navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Sign Out", style: .plain, target: self, action: #selector(handleSignOut))
+    
+    let imageView = UIImageView(image: UIImage(named: "home"))
+    view.addSubview(imageView)
+    _ = imageView.anchor(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: 64, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+  }
+  
+  // MARK: Methods
+  
+  func handleSignOut() {
+    UserDefaults.standard.set(false, forKey: "isLoggedIn")
+    UserDefaults.standard.synchronize()
+    
+    let loginController = LoginViewController()
+    present(loginController, animated: true, completion: nil)
+  }
+}
